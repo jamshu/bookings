57b5e97f790211b1b96589e70de3bf8bcb8be1d1
Jamshid K <jamshid.k@zeeve.io>
2023-02-28 17:58:42 +0530

    json availability and form data post

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -36,7 +37,7 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
-	render.RenderTemplate(w,r, "home.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the handler for the about page
@@ -49,29 +50,29 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap["remote_ip"] = remoteIP
 
 	// send data to the template
-	render.RenderTemplate(w, r,"about.page.tmpl", &models.TemplateData{
+	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
 }
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r,"make-reservation.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
 }
 
 // Generals renders the room page
 func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r,"generals.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "generals.page.tmpl", &models.TemplateData{})
 }
 
 // Majors renders the room page
 func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r,"majors.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "majors.page.tmpl", &models.TemplateData{})
 }
 
 // Availability renders the search availability page
 func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r,"search-availability.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "search-availability.page.tmpl", &models.TemplateData{})
 }
 
 // PostAvailability renders the search availability page
@@ -79,23 +80,26 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	start := r.Form.Get("start")
 	end := r.Form.Get("end")
-	log.Println(r.Form)
-	
-	w.Write([]byte(fmt.Sprintf("start date %s and end date %s",start,end)))
+	log.Println("post form form data,in post availabitilyt",r.Form)
+
+	w.Write([]byte(fmt.Sprintf("start date %s and end date %s", start, end)))
 }
 
 type jsonResponse struct {
-	OK bool `json:"ok"`
+	OK      bool   `json:"ok"`
 	Message string `json:"message"`
 }
+
 // PostAvailability renders the search availability page
 func (m *Repository) AvailabilityJson(w http.ResponseWriter, r *http.Request) {
-	
+
+	data, _ := ioutil.ReadAll(r.Body)
+	log.Println("Json availablitiy",data)
 	resp := jsonResponse{
-		OK:true,
+		OK:      true,
 		Message: "Available",
 	}
-	out,_ :=json.MarshalIndent(resp,"","     ")
+	out, _ := json.MarshalIndent(resp, "", "     ")
 	log.Println(string(out))
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(out)
@@ -103,5 +107,5 @@ func (m *Repository) AvailabilityJson(w http.ResponseWriter, r *http.Request) {
 
 // Contact renders the contact page
 func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w,r, "contact.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "contact.page.tmpl", &models.TemplateData{})
 }
